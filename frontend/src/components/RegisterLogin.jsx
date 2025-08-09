aadd1c49af4199e333b50f15943ba9b9aad2872d
Stefan Savić <stefansavic072@gmail.com>
2025-08-09 23:19:23 +0200

    login page restart

--- frontend/src/components/RegisterLogin.jsx
+++ frontend/src/components/RegisterLogin.jsx
@@ -1,11 +1,10 @@
 import React from "react"
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider, createTheme, styled } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
 
 const darkTheme = createTheme({
     palette: {
@@ -13,37 +12,39 @@ const darkTheme = createTheme({
     },
 });
 
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: (theme.vars ?? theme).palette.text.secondary,
+    ...theme.applyStyles('dark', {
+        backgroundColor: '#1A2027',
+    }),
+}));
+
+
 const RegisterLogin = (props) => {
     return (
-        <React.Fragment>
-            <ThemeProvider theme={darkTheme}>
-                <CssBaseline />
-                <Container maxWidth='sm'>
-                    <Box
-                        component="form"
-                        sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
-                        noValidate
-                        autoComplete="off"
-                    >
-                        <TextField
-                            required
-                            id="outlined-required"
-                            label="Email"
-                            defaultValue=""
-                        /> <TextField
-                            required
-                            id="outlined-password-input"
-                            label="Password"
-                            type="password"
-                            autoComplete="current-password"
-                        />
-                    </Box>
-                    <Grid display="flex" justifyContent="center" alignItems="center">
-                        <Button variant="text">{props.btn}</Button>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container maxWidth={"md"} spacing={2}>
+                    <Grid size={8} display="flex" justifyContent="center" alignItems="center">
+                        <Item>size=8</Item>
                     </Grid>
-                </Container>
-            </ThemeProvider>
-        </React.Fragment>
+                    <Grid size={4} display="flex" justifyContent="center" alignItems="center" size="grow">
+                        <Item>size=4</Item>
+                    </Grid>
+                    <Grid size={4} display="flex" justifyContent="center" alignItems="center" size="grow">
+                        <Item>size=4</Item>
+                    </Grid>
+                    <Grid size={8} display="flex" justifyContent="center" alignItems="center" size="grow">
+                        <Item>size=8</Item>
+                    </Grid>
+                </Grid>
+            </Box>
+        </ThemeProvider>
     )
 }
 
