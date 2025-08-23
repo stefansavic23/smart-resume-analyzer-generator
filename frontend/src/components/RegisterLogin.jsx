1c14f38dfcc1bc5fe6149719f8ec02cb78e6f25f
Stefan Savić <stefansavic072@gmail.com>
2025-08-23 20:14:16 +0200

    revoming item from component

--- frontend/src/components/RegisterLogin.jsx
+++ frontend/src/components/RegisterLogin.jsx
@@ -15,18 +15,6 @@ const darkTheme = createTheme({
     },
 });
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: (theme.vars ?? theme).palette.text.secondary,
-    ...theme.applyStyles('dark', {
-        backgroundColor: '#1A2027',
-    }),
-}));
-
-
 const RegisterLogin = (props) => {
     const [email, setEmailValue] = useState('')
     const [password, setPasswordValue] = useState('')
