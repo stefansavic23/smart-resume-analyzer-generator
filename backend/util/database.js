9bc4166f6261eff579dd29365718ef144696eb96
Stefan Savić <stefansavic072@gmail.com>
2025-08-26 15:15:12 +0200

    security in database.js fixes

--- backend/util/database.js
+++ backend/util/database.js
@@ -2,10 +2,10 @@ import "dotenv/config";
 
 import Sequelize from "sequelize";
 
-const sequelize = new Sequelize('smart_resume', 'sm', process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
+const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT
 });
 
 export default sequelize;
