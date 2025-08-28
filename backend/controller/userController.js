0fc272e2f046dcac91d252259169f9189c8c79d6
Stefan Savić <stefansavic072@gmail.com>
2025-08-28 18:02:38 +0200

    validating email password

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,15 +3,44 @@ import 'dotenv/config'
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 import User from "../model/User.js"
+import validator from "validator"
+import passwordValidator from "password-validator"
+
+const isValidEmail = (email) => {
+    if (validator.isEmail(email) === false) {
+        return res.status(400).json({ message: "Incorrect email" })
+    }
+}
+
+const isValidPassword = (password) => {
+    const passwordSchema = new passwordValidator()
+
+    passwordSchema
+        .is().min(8)
+        .is().max(64)
+        .has().uppercase()
+        .has().lowercase()
+        .has().digits(2)
+        .has().not().spaces()
+
+
+    const result = passwordSchema.validate(password, { details: true })
+
+    if (result.length > 0) {
+        const messages = result.map(r => r.message)
+        return res.status(400).json(messages)
+    }
+}
 
 const login = async (req, res) => {
     try {
         const { email, password } = req.body
-        if ((email === '') || (password === '')) return res.status(401).json({ message: "Please enter email and password" })
+
+        isValidEmail(email)
+        isValidPassword(password)
 
         const userData = { email, password }
         const user = await User.findOne({ where: { email } })
-
 
         if (!user) return res.status(404).json({ message: "User doesn't exist" })
 
@@ -31,6 +60,10 @@ const login = async (req, res) => {
 const register = async (req, res) => {
     try {
         const { email, password } = req.body
+
+        isValidEmail(email)
+        isValidPassword(password)
+
         const hashedPassword = bcrypt.hash(password, 10)
         const userData = { email, hashedPassword }
 
